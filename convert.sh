da09d46019fde02841344c7d0d1f7a09457909ee
Jesse Zhu <jesse@itjesse.com>
2017-07-15 17:37:52 +0900

    update

--- convert.sh
+++ convert.sh
@@ -51,6 +51,7 @@ do
     sed -i "" 's/繁体中文（台湾）/简体中文/g' ./output/$out_filename
     sed -i "" 's/接口缩放比例/窗口缩放比例/g' ./output/$out_filename
     sed -i "" 's/缺省/默认/g' ./output/$out_filename
+    sed -i "" 's/保存空间/存储空间/g' ./output/$out_filename
   fi
 done
 
